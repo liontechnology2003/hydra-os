bc862978bfdca4ba2e4cca19894c07b6c484156d
anass <anass.mestour@gmail.com>
2026-09-14 16:21:57 +0100

    Add build script for HydraOS to automate ISO creation and QEMU launch

---
+++ build.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+set BUILD_DIR=%~dp0
+set QEMU=C:\msys64\mingw64\bin\qemu-system-i386.exe
+
+echo ==========================================
+echo  HydraOS - Build and Run
+echo ==========================================
+
+:: Build the ISO using the MSYS2 toolchain (clang + lld + xorriso)
+echo.
+echo [1/2] Building hydra.iso ...
+C:\msys64\usr\bin\bash.exe -lc "cd '%BUILD_DIR:C:\=/c/%' && make hydra.iso"
+if errorlevel 1 (
+    echo.
+    echo BUILD FAILED.
+    exit /b 1
+)
+
+echo.
+echo [2/2] Launching QEMU ...
+if not exist "%QEMU%" (
+    echo QEMU not found at %QEMU%
+    echo Install it with:  pacman -S mingw-w64-x86_64-qemu
+    exit /b 1
+)
+
+"%QEMU%" -cdrom "%BUILD_DIR%hydra.iso" -m 128 -display sdl -no-reboot
+
+echo.
+echo Done.
+endlocal
